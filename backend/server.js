09900559978f9b8cc7de7e30ebc77219f642c895
Bokam Aravind <137811893+Bokamaravind@users.noreply.github.com>
2025-10-06 12:39:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/dashboardDB';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://bokamaravind_db_user:Aravind@007@cluster0.mfyvep3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 
 mongoose.connect(MONGO_URI)
